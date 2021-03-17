05b7057ce00d71fa96aef740c959ce0c878a35d8
Collins Swai <swailerc23@gmail.com>
2021-03-17 14:08:48 +0300

    Implemented reset password to firebase

---
+++ app/src/main/java/com/example/m_health/UI/ResetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.m_health.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.m_health.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
